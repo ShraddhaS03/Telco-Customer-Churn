69b1363c651072be8e8958c3253dc9145bf66b71
Shraddha Shahane <46230892+ShraddhaS03@users.noreply.github.com>
2019-02-08 15:00:44 -0500

    Add files via upload

---
+++ Telco-Customer-Churn.R
@@ -0,0 +1,12 @@
+#Telco Customer Churn
+
+
+telco <- read.csv("C:\\Users\\sshah\\Desktop\\MVA\\Telco-Customer-Churn.csv")
+class(telco)
+
+# Displaying the contents the data frame containing the imported data
+telco
+# Displaying the first six rows of the data frame euro.emp
+head(telco)
+# Showing the structure of the data frame euro.emp
+str(telco)
