650b51a0934f1ffc72834cb22d2c9efe24ba5035
Shraddha Shahane <46230892+ShraddhaS03@users.noreply.github.com>
2019-02-08 15:18:24 -0500

    Add files via upload

---
+++ Telco-Customer-Churn.R
@@ -0,0 +1,12 @@
+#Telco Customer Churn
+
+
+telco <- read.csv("C:\\Users\\sshah\\Desktop\\MVA\\Telco-Customer-Churn.csv")
+class(telco)
+
+# Displaying the contents the data frame.
+telco
+# Displaying the first six rows of the data frame.
+head(telco)
+# Showing the structure of the data frame.
+str(telco)
